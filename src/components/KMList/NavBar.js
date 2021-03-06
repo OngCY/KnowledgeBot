3b74b6b4212dd147fb8c427f2a909dbacbee9ae9
OngCY <ong.cy@csit.gov.sg>
2021-03-07 00:20:38 +0800

    Styled nav bar

--- src/components/KMList/NavBar.js
+++ src/components/KMList/NavBar.js
@@ -6,17 +6,17 @@ const Navbar = () => {
       <h2>Knowledge Bot</h2>
       <div className="links">
         <Link to="/" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
+          color: 'white',
+          fontWeight: "bold",
+          paddingRight: "1em"
         }}>Main</Link>
         <Link to="/createBot" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
+          color: 'white',
+          fontWeight: "bold",
+          paddingRight: "1em"
         }}>Create Bot</Link>
       </div>
-    </nav>
+    </nav> 
   );
 }
  
